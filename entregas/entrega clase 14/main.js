9ad63935347bb201cacf3b38bcc3f2682eb44fa7
fernando rudnevich <fernandorudnevich.inedita@gmail.com>
2021-11-30 07:12:40 -0300

    modificaion de registros

--- entregas/entrega clase 14/main.js
+++ entregas/entrega clase 14/main.js
@@ -1,7 +1,13 @@
 $(()=>{
     const URL ="usuarios.json";
 
-    $("#ejemploGet").on("click",function(){
+    const request = new XMLHttpRequest();
+    request.open('GET', URL);
+
+    request.responseType = 'json';
+    request.send();
+
+    request.onload = function(){
         let i=1;
         $.get(URL,function(data,status){
             console.log(status);
@@ -23,9 +29,38 @@ $(()=>{
                 }
             }
         })
-    })
+    }
 
   
 
 
 })
+
+//forma de obtener mediante boton
+/* $(()=>{
+  const URL ="usuarios.json";
+
+  $("#ejemploGet").on("click",function(){
+      let i=1;
+      $.get(URL,function(data,status){
+          console.log(status);
+          if(status==="success"){
+              let datos=data;
+              for(const item of datos){
+                  $("#tablaUsuario").append(
+                                  `
+                     <tr>
+                      <th scope="row">${i++}</th>
+                      <td>${item.nombre}</td>
+                      <td>${item.apellido}</td>
+                      <td>${item.dni}</td>
+                      <td>${item.mail}</td>
+                      <td>${item.pass}</td>
+                      
+                    </tr>`
+                  )
+              }
+          }
+      })
+  })
+ */
--- proyecto_final/registrarse.js
+++ proyecto_final/registrarse.js
@@ -78,39 +78,71 @@ const guardar = () => {
   } else {
     localStorage.setItem("usuarios", JSON.stringify(usuarios));
   }
-};
+}
 
 
-//hacemos una tabla con los usuarios
+//hacemos una tabla con los usuarios get
+$(() => {
+  const URL ="usuarios.json";
+/* 
+  const request = new XMLHttpRequest();
+  request.open('GET', URL);
 
-const imprimirDatos = () => {
-  let i=1;
-  verificarSotarage().forEach(obj => {
-    document.getElementById("tablaUsuario").innerHTML += `
-        <tr>
-          <th scope="row">${i++}</th>
-          <td>${obj.nombre}</td>
-          <td>${obj.apellido}</td>
-          <td>${obj.dni}</td>
-          <td>${obj.mail}</td>
-          <td>${obj.pass}</td>
-          
-        </tr>`
-        ;
-  });
-};
+  request.responseType = 'json';
+  request.send();
+
+  request.onload = function(){
+      let i=1;
+      $.get(URL,function(data,status){
+          console.log(status);
+          if(status==="success"){
+              let datos=data;
+              for(const item of datos){
+                  $("#tablaUsuario").append(
+                                  `
+                     <tr>
+                      <th scope="row">${i++}</th>
+                      <td>${item.nombre}</td>
+                      <td>${item.apellido}</td>
+                      <td>${item.dni}</td>
+                      <td>${item.mail}</td>
+                      <td>${item.pass}</td>
+                      
+                    </tr>`
+                  )
+              }
+          }
+      })
+  }
+ */
+  //agrega usuarios Post
+
+  let datosPost={
+    "nombre": "jorge",
+    "apellido": "Palacios",
+    "dni": "2930112021",
+    "mail": "jorge301121@gmail.com",
+    "pass": "123465678"
+}
+$("#btnPost").on("click",function(){
+    $.post(URL,datosPost,function(data,status){
+        alert(`el estado es ${status}`);
+        console.log(`el nomreb es  ${data.nombre}`);
+    })
+})
 
 
+});
 /***********************
       EVENTOS
  **********************/
 //****USUARIOS****/
 
-document.getElementById("btn").addEventListener("click", (e) => {
-e.preventDefault();
-guardar();
+//document.getElementById("btn").addEventListener("click", (e) => {
+//e.preventDefault();
+//guardar();
 
-});
+//});
 
-imprimirDatos();
+//imprimirDatos();
 
